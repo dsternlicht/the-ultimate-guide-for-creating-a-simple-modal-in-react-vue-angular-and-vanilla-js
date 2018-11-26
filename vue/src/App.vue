bad8da0d7d1e7b943c80a6f473c995c0aedcb3fc
Daniel Sternlicht <dsternlicht@obil-dsternlicht.local.il.outbrain.com>
2018-11-26 12:08:07 +0200

    Adding Vue popup

---
+++ vue/src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="app">
+    <button class="popup_opener" v-on:click="togglePopup()">
+      Click Me! I Don't Bite... <span role="img" aria-label="emoji">😛</span>
+    </button>
+
+    <Popup
+      v-bind:show="show"
+      v-bind="{ closeCallback: togglePopup}"
+    >
+      <h2>Told Ya!</h2>
+      <iframe title="giphy" src="https://giphy.com/embed/l52CGyJ4LZPa0" width="480" height="273" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/sandler-sentences-sounding-l52CGyJ4LZPa0">via GIPHY</a></p>
+    </Popup>
+  </div>
+</template>
+
+<script>
+import Popup from './components/Popup.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Popup
+  },
+  data: () => ({
+    show: false
+  }),
+  methods: {
+    togglePopup: function() {
+      this.show = !this.show;
+    }
+  }
+}
+</script>
+
+<style>
+*:focus {
+  outline: 0;
+}
+body {
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  background-image: linear-gradient(120deg, #f1c40f 0%, #f1870f 100%);
+}
+.popup_opener {
+  background: #3498db;
+  padding: 4px 0;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translateX(-50%) translateY(-50%);
+  border-radius: 3px;
+  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  text-align: center;
+  text-transform: uppercase;
+  color: #fff;
+  font-size: 16px;
+  padding: 10px 20px;
+  border: none;
+  transition: background 0.2s ease;
+}
+
+.popup_opener:hover {
+  cursor: pointer;
+  background: #469fdb;
+}
+  
+.popup_opener::after {
+  content: "";
+  display: block;
+  position: absolute;
+  left: 50%;
+  width: 100%;
+  height: 10%;
+  border-radius: 50%;
+  background-color: rgba(0,0,0,0.4);
+  opacity: 0.4;
+  transform: translateX(-50%) translateY(-50%);
+  bottom: -30px;
+}
+</style>
---
+++ vue/src/components/Popup.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="popup" v-bind:style="{ display: show ? 'block' : 'none' }">
+    <div class="overlay" @click="closeCallback()"></div>
+    <div class="popup_content">
+      <slot></slot>
+      <button title="Close" class="close_popup" @click="closeCallback()">
+        <i class="fas fa-times"></i>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Popup',
+  props: {
+    show: Boolean,
+    customClass: String,
+    closeCallback: Function
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.popup {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 990;
+}
+
+.popup .overlay {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 995;
+  background: rgba(0,0,0,0.85);
+}
+
+.popup .popup_content {
+  z-index: 999;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-height: 90%;
+  overflow: auto;
+  background: #fff;
+  padding: 20px;
+  box-shadow: 0 1px 5px rgba(0,0,0,0.7);
+  text-align: center;
+  border-radius: 4px;
+  width: 520px; /* This just a default width */
+}
+
+.popup .popup_content > h2 {
+  font-size: 28px;
+  font-weight: 200;
+  margin: 20px 0 40px;
+  text-align: center;
+}
+
+.popup .popup_content .buttons_wrapper {
+  padding: 20px;
+}
+
+.popup .close_popup {
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  cursor: pointer;
+  font-size: 18px;
+  opacity: 0.5;
+  background: none;
+  border: none;
+  transition: opacity 0.2s ease;
+}
+
+.popup .close_popup:hover {
+  opacity: 0.9;
+}
+</style>
